72d8d20bfe91445e34f39d841bb8b80aa1debaf1
JinSooo <kimjinso@qq.com>
2023-10-22 14:30:48 +0800

    feat: ✨ add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import Header from '@/components/Header/Header'
 import { ThemeProvider } from '@/components/common/ThemeProvider'
 import '@/lib/styles/globals.css'
+import { Metadata } from 'next'
 import { Toaster } from 'react-hot-toast'
+
+export const metadata: Metadata = {
+  title: 'CheatSheet Editor',
+  description: '用于 CheatSheet 的应用快捷键可视化编辑器',
+}
 
 export default function RootLayout({
   children,
